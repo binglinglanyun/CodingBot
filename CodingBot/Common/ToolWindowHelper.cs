a083e738e4aa735191ae8fef22763a982947ae37
Xiangnan Li <xnl@microsoft.com>
2017-07-22 13:19:55 +0800

    Create CodingBot Project

---
+++ CodingBot/Common/ToolWindowHelper.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Shell.Interop;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingBot.Common
+{
+    public static class ToolWindowHelper
+    {
+        public static void ShowToolWindow(Package package, Type toolWindowType)
+        {
+            // Get the instance number 0 of this tool window. This window is single instance so this instance
+            // is actually the only one.
+            // The last flag is set to true so that if the tool window does not exists it will be created.
+            ToolWindowPane window = package.FindToolWindow(toolWindowType, 0, true);
+            if ((null == window) || (null == window.Frame))
+            {
+                throw new NotSupportedException("Cannot create tool window");
+            }
+
+            IVsWindowFrame windowFrame = (IVsWindowFrame)window.Frame;
+            Microsoft.VisualStudio.ErrorHandler.ThrowOnFailure(windowFrame.Show());
+        }
+    }
+}
---
+++ CodingBot/ToolWindows/ConversationToolWindow.cs
@@ -0,0 +1,29 @@
+﻿
+using CodingBot.Controls;
+using Microsoft.VisualStudio.Shell;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingBot.ToolWindows
+{
+    [Guid("9ff68ee2-64c8-4223-ade0-9eb6ae79baba")]
+    public class ConversationToolWindow : ToolWindowPane
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CodingBot"/> class.
+        /// </summary>
+        public ConversationToolWindow() : base(null)
+        {
+            this.Caption = "Conversation";
+
+            // This is the user control hosted by the tool window; Note that, even if this class implements IDisposable,
+            // we are not calling Dispose on this object. This is because ToolWindowPane calls Dispose on
+            // the object returned by the Content property.
+            this.Content = new ConversationControl();
+        }
+    }
+}
