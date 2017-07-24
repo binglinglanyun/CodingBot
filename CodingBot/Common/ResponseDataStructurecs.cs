ff82c7cd2bcc916e501348f3b728f8ec84697c76
Xiangnan Li <xnl@microsoft.com>
2017-07-24 15:36:51 +0800

    Define interface that client needs

---
+++ CodingBot/Common/ResponseDataStructurecs.cs
@@ -0,0 +1,25 @@
+﻿using CodingBot.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingBot.Common
+{
+    public enum EnumContorlType
+    {
+        RadioBox = 0,
+        CheckBox = 1,
+        Others = 2
+    }
+
+    public class ResponseData
+    {
+        public string SciptContent;
+        public string BotMessage;
+
+        EnumContorlType ControlType;
+        public List<TableItem> TableItems;
+    }
+}
